30d94ceb4988351966698b47d3eb634f3eeadd49
Gabriel Malheiro <g.silva.malheiro@avanade.com>
2023-01-02 01:25:39 -0300

    Minimal API is complete!

--- SecretAgentAPI/Program.cs
+++ SecretAgentAPI/Program.cs
@@ -1,28 +1,61 @@
 using Microsoft.EntityFrameworkCore;
 
-var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args); // [5] builder.Services.AddAuthorization();
 
-// Add services to the container.
-builder.Services.AddAuthorization();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseInMemoryDatabase("AgentsDB"));
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.MapGet("/ListAgents", async (AppDbContext db) => await db.Agents.ToListAsync());
 
+app.MapGet("/ListAgentById", async (int id, AppDbContext db) =>
+                             await db.Agents.FindAsync(id) is Agent agent ? Results.Ok(agent) : Results.NotFound("The agent you are looking for doesnt seems to be in our system")) ;
 
+app.MapGet("/ListAgents/Active", async (AppDbContext db) =>
+                                   await db.Agents.Where(a => a.IsActive).ToListAsync());
+
+app.MapPost("/CreateAgent", async (AppDbContext db, Agent agent) =>
+{
+    db.Agents.Add(agent);
+    await db.SaveChangesAsync();
+    return Results.Created($"/Agents/{agent.Id}",agent);
+} );
+
+app.MapPut("/AlterAgent", async (int id, Agent agentInput, AppDbContext db) =>
+{
+    var agent = await db.Agents.FindAsync(id);
+
+    if (agent is null)
+        return Results.NotFound("The agent your trying to edit doesnt seem to be in our system...");
+
+    agent.Name= agentInput.Name;
+    agent.IsActive=agentInput.IsActive;
+
+    await db.SaveChangesAsync();
+    return Results.NoContent();
+});
+
+app.MapDelete("/DeleteAgent", async (int id, AppDbContext db) =>
+{
+    if(await db.Agents.FindAsync(id) is Agent agent)
+    {
+        db.Agents.Remove(agent);
+        await db.SaveChangesAsync();
+        return Results.Ok(agent);
+    }
+    return Results.NotFound("The agent your looking for doesnt seem to exist in our database...");
+});
 
 app.Run();
 
@@ -30,7 +63,7 @@ class Agent
 {
     public int Id { get; set; }
     public string? Name { get; set; }
-    public bool IsActived { get; set; } 
+    public bool IsActive { get; set; } 
 
 }
 
